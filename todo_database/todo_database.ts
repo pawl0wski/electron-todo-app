9abb19a61dc79f3f6636fa519da26f0c3e0a046f
Jebok <kanakuba123359@gmail.com>
2021-11-30 23:22:10 +0100

    Create TodoDatabase

---
+++ todo_database/todo_database.ts
@@ -0,0 +1,48 @@
+import { open, Database } from "sqlite";
+import * as sqlite3 from "sqlite3";
+
+export class TodoDatabase {
+    db: Database;
+    constructor() {
+
+    }
+
+    async openDB(dbPath: string){
+        this.db = await open({
+            filename: dbPath,
+            driver: sqlite3.Database,
+        });
+        await this._createTable();
+    }
+
+    async _createTable() {
+        await this.db.run(
+            "CREATE TABLE IF NOT EXISTS todo (id INTEGER PRIMARY KEY, content TEXT)"
+        );
+    }
+
+    async insertTodo(content: string): Promise<number> {
+        let runResult  = await this.db.run(
+            "INSERT INTO todo (content) VALUES (?)",
+            [content],
+        )
+        return runResult.lastID;
+    }
+
+    deleteTodo(id: number) {
+        this.db.run(
+            "DELETE FROM todo WHERE id = ?",
+            [id],
+        );
+    }
+
+    async getTodos(): Promise<{ id: any; content: any; }[]>{
+        let todoList: { id: any; content: any; }[] = [];
+
+        await this.db.each("SELECT * FROM todos;", (e, row) => {
+            todoList.push({id: row["id"], content: row["content"]});
+        });
+
+        return todoList;
+    }
+}
